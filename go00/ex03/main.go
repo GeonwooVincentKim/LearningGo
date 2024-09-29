9bc43341e332fdfa754300f77faeacf491d97853
GeonwooVincentKim <kdsnop@gmail.com>
2024-09-29 20:43:32 +0900

    [Update] - Another Go code

---
+++ go00/ex03/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"ex03/piscine"
+)
+
+func main() {
+	piscine.IsNegative(0)
+	piscine.IsNegative(1)
+	piscine.IsNegative(-1)
+}
---
+++ go00/ex03/piscine/isnegative.go
@@ -0,0 +1,11 @@
+package piscine
+
+import "fmt"
+
+func IsNegative(n int) {
+	if n < 0 {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+}

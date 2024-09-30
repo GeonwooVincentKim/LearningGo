4a28cd40ae06c38a46f4b3dddb195755052c8b47
GeonwooVincentKim <kdsnop@gmail.com>
2024-09-30 19:41:33 +0900

    [Feature] - Sqrt

---
+++ go01/ex05/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"ex05/sqrt"
+	"fmt"
+)
+
+func main() {
+	arg := 4
+	fmt.Println(sqrt.Sqrt(arg))
+	fmt.Println(sqrt.Sqrt(0))
+}
---
+++ go01/ex05/sqrt/sqrt.go
@@ -0,0 +1,7 @@
+package sqrt
+
+import "math"
+
+func Sqrt(nb int) int {
+	return int(math.Sqrt(float64(nb)))
+}

0de9360fbd632aa4047ac676d60df5f0a2746a39
GeonwooVincentKim <kdsnop@gmail.com>
2024-09-30 19:47:03 +0900

    [Feature] - IsPrime:

---
+++ go01/ex06/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"ex06/isprime"
+)
+
+func main() {
+	fmt.Println(isprime.IsPrime(5))
+	fmt.Println(isprime.IsPrime(4))
+	fmt.Println(isprime.IsPrime(7))
+	fmt.Println(isprime.IsPrime(11))
+	fmt.Println(isprime.IsPrime(13))
+	fmt.Println(isprime.IsPrime(17))
+	fmt.Println(isprime.IsPrime(19))
+	fmt.Println(isprime.IsPrime(23))
+	fmt.Println(isprime.IsPrime(29))
+	fmt.Println(isprime.IsPrime(31))
+	fmt.Println(isprime.IsPrime(37))
+}

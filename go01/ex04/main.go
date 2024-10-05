7fe4f9f9c8e482572260c05c42764a7b2bce7b43
GeonwooVincentKim <kdsnop@gmail.com>
2024-10-05 15:40:56 +0900

    [Feature] - Main

---
+++ go01/ex04/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"ex04/printstr"
+	"fmt"
+)
+
+func main() {
+	s := "Hello World"
+	fmt.Println(printstr.PrintStr(s))
+}
---
+++ go01/ex04/printstr/printstr.go
@@ -0,0 +1,22 @@
+package printstr
+
+import "fmt"
+
+func GetStrLen(s string) int {
+	count := 0
+	for range s {
+		count++
+	}
+	return count
+}
+
+func PrintStr(s string) string {
+	// Convert string to slice of runes
+	rsStr := []rune(s) // Use []runes to correctly handle multibytes character
+
+	for i := 0; i < GetStrLen(s); i++ {
+		fmt.Println("Get S Value - ", rsStr[i])
+		fmt.Println("Get S Address - \n", &rsStr[i])
+	}
+	return s
+}
